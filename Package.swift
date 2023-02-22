4ddeeabd766cfcad0ced06e31035888c915be1ca
jake@showpo.com <jake@showpo.com>
2023-02-22 13:23:39 +0800

    Updated minimum supported version for iOS and tvOS

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "ScrollViewProxy",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11),
-        .tvOS(.v11)
+        .iOS(.v13),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
